06e024131bbb31683d5c0fd9ef4de0a235774758
collinsr729 <collinsr729@yahoo.com>
2016-02-13 11:06:21 -0500

    :cat: :gun: Added I2C for future rioduino integration
    Added Arduino Class

---
+++ src/org/usfirst/frc/team1559/robot/Arduino.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team1559.robot;
+import edu.wpi.first.wpilibj.I2C;
+
+public class Arduino {
+	I2C arduino;
+	byte[] data;
+	int address;
+	int value1 = 0;
+	int value2 = 0;
+	int value3 = 0;
+
+	public Arduino (int address_){
+		arduino = new I2C(I2C.Port.kMXP,address_);
+		address_ = address;
+		data = new byte[3];
+	}
+	private void write(int val1, int val2, int val3){
+		data[0] = (byte) val1;
+		data[1] = (byte) val2;
+		data[2] = (byte) val3;
+		arduino.writeBulk(data);
+	}
+	public void writeSequence(int sequence){
+		sequence = value1;
+		write(value1,value2,value3);
+	}
+	public void writeElevatorPos(int elevatorPos){
+		elevatorPos = value2;
+		write(value1,value2,value3);
+	}
+	public void writeAlliance(int alliance){
+		alliance = value3;
+		write(value1,value2,value3);
+	}
+}
