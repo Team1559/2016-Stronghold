5cfa292c406e13ef9bdebe1af29c9d81f8916e91
School Computer <1559DevilTech@gmail.com>
2016-03-25 15:09:05 -0400

    Recording in test mode

--- src/org/usfirst/frc/team1559/robot/Wiring.java
+++ src/org/usfirst/frc/team1559/robot/Wiring.java
@@ -66,7 +66,7 @@ public class Wiring {
 	public static final int CLAMP_RIGHT_ID = 6;
 	public static final int CLAMP_BALL_SENSOR_ID = 0;
 	public static final double CLAMP_SENSOR_THRESHOLD = 2.5;
-	public static final double CLAMP_LOW = 2.8;
+	public static final double CLAMP_LOW = 2.95;
 	public static final double CLAMP_HIGH = 3.2;
 	
 	// gatherer constants
